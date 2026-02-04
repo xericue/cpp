bec4320419433d248d41f4cbf58c76da4611c957
Kristofer Ulanday <kulanday@nd.edu>
2026-02-04 10:26:47 -0500

    Genuinely why is this not running

--- bruh.cpp
+++ bruh.cpp
@@ -1,17 +1,36 @@
-#include <iostream>
-#include <thread>
+// #include <iostream>
 
-class Person { // okay here's a class!
-  public:
-    Person(std::string &name);
+// // class Person { // okay here's a class!
+// //   public:
+// //     Person(std::string &name);
 
-  private:
-    int age;
-    int income;
-};
+// //   private:
+// //     int age;
+// //     int income;
+// // };
 
-int main() {
-  std::cout << "hi";
+// int main() {
+//   std::cout << "hi";
   
-  return 0;
+//   return 0;
+// }
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+
+    cin.get();
+    return 0;
 }
