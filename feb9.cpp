5d0c95aab5a09f8ed300081ec3f39a32cffd67bf
xericue <106630482+xericue@users.noreply.github.com>
2026-02-09 13:42:59 +0000

    cpp basic printing w/ stl and cout = character output

---
+++ feb9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    // special function called main xd
+
+    // std = namespace of standard, basically everything in C++
+    // << is how you  display something to console. printf, print, etc
+    // i think its the insertion operator for data stream to output
+    std::cout << "bruh\n"; // ooh you do be needing a new line...
+    std::cout << "6.7\n";
+    std::cout << "d\n";
+    std::cout << "kvjnq34kj1;4rn1k4flkj3x4nr1h2kr1\\rvkjnefvmwkerv/ervsfdvafkvj24k;lfjlkwrmv98y9)*hohJL;JKLJH98Y32HIRJ1)(*^@$)*&^!*(&%$!(&#^))\n";
+
+
+    /* ngggghhhh
+    hi!
+    okay br
+    */
+
+    // Ok we gettin into objects
+    return 0;
+}
