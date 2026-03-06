72e91e4bb77e51d3d1c4d6b1517d0d8a7f3636fd
xericue <106630482+xericue@users.noreply.github.com>
2026-03-06 01:54:30 +0000

    1.5

---
+++ r105.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+    // always needs iostream - #include <iostream>
+    // always need a main function
+    // g++ r105.cpp - compile with g++, not gcc
+
+    int x {5};
+
+    // << is insertion operator
+
+    // this is all buffered - its not sent to the console immediately
+    // first its put "in line" in an intermediate memory area called the
+    // stream output buffer, literally internally "std::streambuf"
+    
+    std::cout << x << std::endl;
+    std::cout << "yo gurt" << std::endl;
+    std::cout << "yo " << std::endl << "gurt" << std::endl;
+
+
+    return 0;
+}
