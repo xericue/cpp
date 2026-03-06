360f1891396e99c81951c790858b3a55df873db3
xericue <106630482+xericue@users.noreply.github.com>
2026-03-06 01:55:59 +0000

    endl is slow because it unnecessarily flushes

--- r105.cpp
+++ r105.cpp
@@ -13,11 +13,17 @@ int main() {
     // this is all buffered - its not sent to the console immediately
     // first its put "in line" in an intermediate memory area called the
     // stream output buffer, literally internally "std::streambuf"
-    
+
     std::cout << x << std::endl;
     std::cout << "yo gurt" << std::endl;
     std::cout << "yo " << std::endl << "gurt" << std::endl;
 
+    // OOPS USING std::endl IS SLOW BECAUSE IT ALSO FLUSHES - we dont need to do that
+    // flushing is slow
+
+    std::cout << x << '\n';
+    std::cout << "yo gurt" << '\n';
+    std::cout << "yo \ngurt\n";
 
     return 0;
 }
