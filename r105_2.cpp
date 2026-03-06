24ec1d47b2220ef68169c48212d128e25c0e1855
xericue <106630482+xericue@users.noreply.github.com>
2026-03-06 20:46:31 +0000

    1.5 2

---
+++ r105_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int x {}, y {};
+
+    std::cout << "type a number yo\n";
+
+    std::cin >> x >> y;
+
+    std::cout << "twin you really printed " << x << " and " << y << "...\n";
+
+    return 0;
+}
