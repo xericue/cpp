79952763dd02bfe3fefa1f4661804663d3d6f5e7
xericue <106630482+xericue@users.noreply.github.com>
2026-03-07 18:43:10 +0000

    structs are public by default in C++; access attributes with . and use Struct name{ attributes };

--- mar7/classes.cpp
+++ mar7/classes.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
 struct Dog {
+    // members of a struct are public by default in C++
     int age {};
 };
 
 int main() {
     std::cout << "hi\n";
+
+    Dog new_new{1};
+
+    std::cout << new_new.age << '\n';
     return 0;
 
-    Dog bruh{};
-    Dog new_new{1};
 }
+
+#include <iostream>
